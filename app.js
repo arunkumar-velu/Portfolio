6ee82ee68ae68d86660e29eee09e481bcccf8a3d
Arun <arunmadcoder@gmail.com>
2016-08-24 23:41:18 +0530

    Node server added

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 8000));
+app.use("/assets", express.static(__dirname + '/assets'));
+app.use("/images", express.static(__dirname + '/images'));
+
+app.get('/', function(req, res){
+   res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(app.get('port'), function () {
+	console.log('Example app listening on port 8000!');
+});
+
