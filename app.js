085202de7c11d5a0bdf781d897aaf4bdac896922
Arun Kumar Velu <arunkumar.velu@freshworks.com>
2021-09-16 12:27:11 +0530

    Local server path changes

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use("/assets", express.static(__dirname + '/assets'));
-app.use("/images", express.static(__dirname + '/images'));
+app.use("/assets", express.static(__dirname + '/public/assets'));
+app.use("/images", express.static(__dirname + '/public/images'));
 
-app.get('/', function(req, res){
-   res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+   res.sendFile(__dirname + '/public/index.html');
 });
 app.listen(app.get('port'), function () {
-	console.log('Example app listening on port 5000!');
+   console.log('Example app listening on port 5000!');
 });
 
